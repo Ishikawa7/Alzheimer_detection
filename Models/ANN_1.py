f4c0d8844574ae7b0911fd33a64356ca9fc35ad3
Ishikawa7 <davidegrimaldi92@gmail.com>
2023-10-01 16:01:26 +0200

    testing

--- Models/ANN_1.py
+++ Models/ANN_1.py
@@ -5,28 +5,31 @@ from tensorflow.keras.models import Model
 def build_Model(input_shape : tuple[int]) -> Model:
     input = layers.Input(shape= input_shape ,name="input")
     flatten = layers.Flatten()(input)
-    dense_0 = layers.Dense(500, activation="gelu")(flatten)
-    #dropout_0 = layers.Dropout(0.2)(dense_0)
-    dense_1 = layers.Dense(500, activation="gelu")(dense_0)
-    #dropout_1 = layers.Dropout(0.2)(dense_1)
-    dense_2 = layers.Dense(500, activation="gelu")(dense_1)
+    dense_0 = layers.Dense(300, activation="tanh")(flatten)
+    dropout_0 = layers.Dropout(0.3)(dense_0)
+    dense_1 = layers.Dense(300, activation="tanh")(dropout_0)
+    dropout_1 = layers.Dropout(0.3)(dense_1)
+    ##dense_2 = layers.Dense(729, activation="tanh")(dense_1)
     #dropout_2 = layers.Dropout(0.2)(dense_2)
-    dense_3 = layers.Dense(500, activation="gelu")(dense_2)
+    ##dense_3 = layers.Dense(729, activation="tanh")(dense_2)
     #dropout_3 = layers.Dropout(0.2)(dense_3)
-    dense_4 = layers.Dense(250, activation="gelu")(dense_3)
-    #dropout_4 = layers.Dropout(0.2)(dense_4)
-    dense_5 = layers.Dense(100, activation="gelu")(dense_4)
-    #dropout_5 = layers.Dropout(0.2)(dense_5)
-    output = layers.Dense(1, activation="gelu", name="output")(dense_5)
+    dense_4 = layers.Dense(200, activation="tanh")(dropout_1)
+    dropout_4 = layers.Dropout(0.2)(dense_4)
+    dense_5 = layers.Dense(100, activation="tanh", kernel_regularizer='l1')(dropout_4) #
+    dropout_5 = layers.Dropout(0.3)(dense_5)
+
+    #output = layers.Dense(1, activation="relu", name="output")(dense_5)
+    output_softmax = layers.Dense(2, activation="softmax", kernel_regularizer='l1')(dropout_5)
 
     model = Model(
         inputs = [input],
-        outputs = [output]
+        outputs = [output_softmax]
         )
 
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-        loss=["mse"],
-        loss_weights=[1.0],
-        )
+    #model.compile(
+    #    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    #    loss=["mse"],
+    #    loss_weights=[1.0],
+    #    )
+    model.compile(optimizer='SGD', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
     return model
--- Models/ConvMassimo.py
+++ Models/ConvMassimo.py
@@ -6,15 +6,48 @@ from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Averag
 def build_Model(input_shape : tuple[int]) -> Sequential:
     model = Sequential()
     model.model_name = "ConvMassimo"
-    model.add(Conv2D(filters = 16, kernel_size=7, activation="gelu", input_shape=input_shape))
+    model.add(Conv2D(filters = 16, kernel_size=4, activation="relu", input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(filters = 32, kernel_size=7, activation="gelu"))
+    model.add(Conv2D(filters = 32, kernel_size=4, activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters = 64, kernel_size=4, activation="relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
-    model.add(Dense(200, activation="gelu"))
+    model.add(Dense(200, activation="relu"))
     model.add(Dropout(0.2))
-    model.add(Dense(100, activation="gelu"))
+    model.add(Dense(100, activation="relu"))
     model.add(Dropout(0.2))
-    model.add(Dense(1, activation="relu"))
-    model.compile(optimizer="adam", loss="mse")
+    #model.add(Dense(1, activation="relu"))
+    model.add(Dense(4, activation="softmax"))
+    #model.compile(optimizer="adam", loss="mse")
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
     return model
+
+#   model = Sequential()
+#   #add model layers - RP Architettura
+#   model.add(Conv2D(16, kernel_size=1, activation="relu", input_shape=(img_height, img_width, num_channels)))
+#   model.add(Conv2D(16, kernel_size=3, activation="relu"))
+#   model.add(MaxPooling2D(pool_size=(2, 2)))
+#   model.add(Conv2D(16, kernel_size=3, activation="relu")) #3
+#   model.add(Conv2D(32, kernel_size=3, activation="relu")) #3
+#   model.add(MaxPooling2D(pool_size=(2, 2)))
+#   model.add(Conv2D(32, kernel_size=3, activation="relu")) #3
+#   model.add(Conv2D(64, kernel_size=3, activation="relu")) #3
+#   model.add(MaxPooling2D(pool_size=(2, 2)))
+#   model.add(Conv2D(64, kernel_size=3, activation="relu")) #3
+#   model.add(Conv2D((96), kernel_size=3, activation="relu")) #3
+#   model.add(MaxPooling2D(pool_size=(2, 2)))
+#   #model.add(Conv2D(64, kernel_size=3, activation="relu")) #3
+#   #model.add(Conv2D(128, kernel_size=3, activation="relu")) #3
+#   #model.add(MaxPooling2D(pool_size=(2, 2)))
+#   #model.add(Conv2D(15, kernel_size=3,  padding = 'SAME', activation="relu")) #3
+#   # model.add(MaxPooling2D(pool_size=(2, 2)))
+#   # model.add(Conv2D(10, kernel_size=3, activation="relu")) #3
+#   # model.add(MaxPooling2D(pool_size=(2, 2)))
+#   # model.add(Conv2D(15, kernel_size=3, activation="relu"))
+#   model.add(Flatten())
+#   model.add(Dense(200, activation="relu"))
+#   model.add(Dropout(0.4))
+#   model.add(Dense(100, activation="relu"))
+#   model.add(Dropout(0.4))
+#   model.add(Dense(Class_Count, activation="softmax"))
