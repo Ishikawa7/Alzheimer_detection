e4d194b1d92560e9e5eee4328d67a4983ebb2472
Ishikawa7 <davidegrimaldi92@gmail.com>
2023-10-02 11:21:13 +0200

    testing sample weights

--- Models/ANN_1.py
+++ Models/ANN_1.py
@@ -5,21 +5,21 @@ from tensorflow.keras.models import Model
 def build_Model(input_shape : tuple[int]) -> Model:
     input = layers.Input(shape= input_shape ,name="input")
     flatten = layers.Flatten()(input)
-    dense_0 = layers.Dense(300, activation="tanh")(flatten)
-    dropout_0 = layers.Dropout(0.3)(dense_0)
-    dense_1 = layers.Dense(300, activation="tanh")(dropout_0)
-    dropout_1 = layers.Dropout(0.3)(dense_1)
-    ##dense_2 = layers.Dense(729, activation="tanh")(dense_1)
-    #dropout_2 = layers.Dropout(0.2)(dense_2)
-    ##dense_3 = layers.Dense(729, activation="tanh")(dense_2)
-    #dropout_3 = layers.Dropout(0.2)(dense_3)
-    dense_4 = layers.Dense(200, activation="tanh")(dropout_1)
-    dropout_4 = layers.Dropout(0.2)(dense_4)
-    dense_5 = layers.Dense(100, activation="tanh", kernel_regularizer='l1')(dropout_4) #
-    dropout_5 = layers.Dropout(0.3)(dense_5)
+    dense_0 = layers.Dense(729, activation="tanh")(flatten)
+    dropout_0 = layers.Dropout(0.0)(dense_0)
+    dense_1 = layers.Dense(500, activation="tanh")(dropout_0)
+    dropout_1 = layers.Dropout(0.0)(dense_1)
+    dense_2 = layers.Dense(400, activation="tanh")(dropout_1)
+    dropout_2 = layers.Dropout(0.0)(dense_2)
+    dense_3 = layers.Dense(300, activation="tanh")(dropout_2)
+    dropout_3 = layers.Dropout(0.0)(dense_3)
+    dense_4 = layers.Dense(200, activation="tanh")(dropout_3)
+    dropout_4 = layers.Dropout(0.0)(dense_4)
+    dense_5 = layers.Dense(100, activation="tanh")(dropout_4)
+    dropout_5 = layers.Dropout(0.0)(dense_5)
 
     #output = layers.Dense(1, activation="relu", name="output")(dense_5)
-    output_softmax = layers.Dense(2, activation="softmax", kernel_regularizer='l1')(dropout_5)
+    output_softmax = layers.Dense(4, activation="softmax")(dropout_5) #, kernel_regularizer='l1'
 
     model = Model(
         inputs = [input],
@@ -31,5 +31,9 @@ def build_Model(input_shape : tuple[int]) -> Model:
     #    loss=["mse"],
     #    loss_weights=[1.0],
     #    )
-    model.compile(optimizer='SGD', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+    model.compile(
+        #optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001),
+        loss=tf.keras.losses.CategoricalFocalCrossentropy(), 
+        metrics=['categorical_accuracy'])
     return model
